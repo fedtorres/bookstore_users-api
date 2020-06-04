a2bbdd808eebf9e83049c88116256d801589d016
Federico Santiago Torres <federicosantiago.torres@mercadolibre.com>
2020-06-03 23:42:30 -0300

    users structure

--- app/application.go
+++ app/application.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"github.com/fedtorres/bookstore_users-api/logger"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,5 +11,7 @@ var (
 
 func StartApplication() {
 	mapUrls()
+
+	logger.Info("about to start the application...")
 	router.Run(":8080")
 }
--- utils/errors/rest_error.go
+++ utils/errors/rest_error.go
@@ -1,6 +1,7 @@
 package errors
 
 import (
+	"errors"
 	"net/http"
 )
 
@@ -8,6 +9,10 @@ type RestErr struct {
 	Message string `json:"message"`
 	Status  int    `json:"status"`
 	Error   string `json:"error"`
+}
+
+func NewError(msg string) error {
+	return errors.New(msg)
 }
 
 func NewBadRequestError(message string) *RestErr {
@@ -23,5 +28,13 @@ func NewNotFoundError(message string) *RestErr {
 		Message: message,
 		Status:  http.StatusNotFound,
 		Error:   "not_found",
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   "internal_server_error",
 	}
 }
